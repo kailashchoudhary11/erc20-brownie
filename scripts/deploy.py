40a0d7020356a6fe15dc53a29b1cd53a112a21a1
kai <kailashjat604@gmail.com>
2023-02-17 11:40:00 +0530

    Deployed on goerli

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,12 @@
-from brownie import OurToken
+from brownie import OurToken, web3
 from scripts.helpers import get_account
+
+INITIAL_SUPPLY = web3.toWei(1000, "ether")
 
 def deploy():
     print("Deploying OurToken...")
     account = get_account()
-    OurToken.deploy(100, {"from": account})
+    OurToken.deploy(INITIAL_SUPPLY, {"from": account})
     print("Deployed OurToken!")
 
 def main():
